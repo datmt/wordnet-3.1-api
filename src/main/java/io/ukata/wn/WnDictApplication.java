15446b107350ab9c8bb32449c966f86e66fcc8e4
Mạnh Đạt <dat.tm24@gmail.com>
2023-06-12 11:51:58 +0700

    start script

---
+++ src/main/java/io/ukata/wn/WnDictApplication.java
@@ -0,0 +1,13 @@
+package io.ukata.wn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnDictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WnDictApplication.class, args);
+    }
+
+}
