5ac30d4397d83524518a9f2428e80896f755b726
Mạnh Đạt <dat.tm24@gmail.com>
2023-06-12 11:49:21 +0700

    fake test

---
+++ src/test/java/io/ukata/wn/WnDictApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ukata.wn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WnDictApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
